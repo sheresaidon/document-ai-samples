5ca06c76bb6d3d67cf4c57648f081c92bd1e4607
shere <sheresaidon@gmail.com>
2023-06-08 10:20:00 -0400

    update main.py and main.py

--- pdf-embedded-text/main.py
+++ pdf-embedded-text/main.py
@@ -1,4 +1,9 @@
-from typing import Sequence
+
+from typing import List, Sequence
+from pdf2image import convert_from_path
+from PIL import Image, ImageDraw
+import os
+from PyPDF2 import PdfWriter, PdfReader
 
 from google.api_core.client_options import ClientOptions
 from google.cloud import documentai_v1beta3 as documentai
@@ -37,12 +42,14 @@ def process_document_ocr_sample(
         print_detected_langauges(page.detected_languages)
         print_paragraphs(page.paragraphs, text)
         print_blocks(page.blocks, text)
-        print_lines(page.lines, text)
+        print_lines(page.lines, text, page.tables)
         print_tokens(page.tokens, text)
 
         # Currently supported in version pretrained-ocr-v1.1-2022-09-12
         if page.image_quality_scores:
             print_image_quality_scores(page.image_quality_scores)
+    
+    save_annotated_pdf(document, input_pdf_path, output_pdf_path)
 
 
 def process_document(
@@ -121,7 +128,7 @@ def print_blocks(blocks: Sequence[documentai.Document.Page.Block], text: str) ->
     print(f"        Last text block: {repr(last_block_text)}")
 
 
-def print_lines(lines: Sequence[documentai.Document.Page.Line], text: str) -> None:
+def print_lines(lines: Sequence[documentai.Document.Page.Line], text: str, tables: Sequence[documentai.Document.Page.Table]) -> None:
     print(f"    {len(lines)} lines detected:")
 
     # Sort lines by their y position
@@ -176,6 +183,18 @@ def print_lines(lines: Sequence[documentai.Document.Page.Line], text: str) -> No
                 line_texts.append(line_text)
         print("".join(line_texts))
 
+    # Print tables
+    if tables:
+        print("                 Tables:")
+        for table_idx, table in enumerate(tables):
+            print(f"        Table {table_idx + 1}:")
+            header_row_values = get_table_data(table.header_rows, text)
+            body_row_values = get_table_data(table.body_rows, text)
+            all_rows = header_row_values + body_row_values
+            for row in all_rows:
+                print("\t".join(row))
+
+
 def print_tokens(tokens: Sequence[documentai.Document.Page.Token], text: str) -> None:
     print(f"    {len(tokens)} tokens detected:")
     first_token_text = layout_to_text(tokens[0].layout, text)
@@ -210,18 +229,102 @@ def layout_to_text(layout: documentai.Document.Page.Layout, text: str) -> str:
     for segment in layout.text_anchor.text_segments:
         start_index = int(segment.start_index)
         end_index = int(segment.end_index)
-        response += text[start_index:end_index].replace('\n', ' ')
+        response += text[start_index:end_index].replace('\n', '  ')
     return response
 
+def get_table_data(
+    rows: Sequence[documentai.Document.Page.Table.TableRow], text: str
+) -> List[List[str]]:
+    """
+    Get Text data from table rows
+    """
+    all_values: List[List[str]] = []
+    for row in rows:
+        current_row_values: List[str] = []
+        for cell in row.cells:
+            current_row_values.append(
+                text_anchor_to_text(cell.layout.text_anchor, text)
+            )
+        all_values.append(current_row_values)
+    return all_values
+
+def text_anchor_to_text(text_anchor: documentai.Document.TextAnchor, text: str) -> str:
+    """
+    Document AI identifies table data by their offsets in the entirity of the
+    document's text. This function converts offsets to a string.
+    """
+    response = ""
+    # If a text segment spans several lines, it will
+    # be stored in different text segments.
+    for segment in text_anchor.text_segments:
+        start_index = int(segment.start_index)
+        end_index = int(segment.end_index)
+        response += text[start_index:end_index]
+    return response.strip().replace("\n", "  ")
+
+def save_annotated_pdf(document: documentai.Document, input_pdf_path: str, output_pdf_path: str) -> None:
+    # Iterate through the pages and layout elements
+    for page in document.pages:
+        # Calculate the size of the output image based on the dimensions of the PDF page
+        page_width = int(page.dimension.width)
+        page_height = int(page.dimension.height)
+        size = (page_width, page_height)
+
+        # Convert the input PDF to an image with the specified size
+        pdf_image = convert_from_path(input_pdf_path, size=size, first_page=page.page_number, last_page=page.page_number)[0]
+        draw = ImageDraw.Draw(pdf_image)
+        # Helper function to draw bounding boxes
+        def draw_bounding_box(layout, color):
+            vertices = layout.bounding_poly.vertices
+            if len(vertices) == 4:
+                draw.polygon([
+                    (vertices[0].x, vertices[0].y),
+                    (vertices[1].x, vertices[1].y),
+                    (vertices[2].x, vertices[2].y),
+                    (vertices[3].x, vertices[3].y)
+                ], outline=color,
+                             width=3)
+
+        # Draw bounding boxes for paragraphs, blocks, lines, and tokens
+        for paragraph in page.paragraphs:
+            draw_bounding_box(paragraph.layout, 'red')
+        for block in page.blocks:
+            draw_bounding_box(block.layout, 'blue')
+        for line in page.lines:
+            draw_bounding_box(line.layout, 'green')
+        for token in page.tokens:
+            draw_bounding_box(token.layout, 'yellow')
+
+
+        # Save the annotated image as a PDF
+        pdf_image.save(f"{output_pdf_path[:-4]}_page{page.page_number}.pdf", "PDF")
+
+    # Combine the individual PDF pages into a single PDF
+    pdf_writer = PdfWriter()
+    for page_number in range(1, len(document.pages) + 1):
+        with open(f"{output_pdf_path[:-4]}_page{page_number}.pdf", "rb") as page_file:
+            pdf_writer.add_page(PdfReader(page_file).pages[0])
+
+    # Save the combined PDF
+    with open(output_pdf_path, "wb") as output_file:
+        pdf_writer.write(output_file)
+
+    # Remove the individual PDF pages
+    for page_number in range(1, len(document.pages) + 1):
+        os.remove(f"{output_pdf_path[:-4]}_page{page_number}.pdf")
 
 # TODO(developer): Edit these variables before running the sample.
 project_id = "bright-black-ai"
 location = "us"  # Format is 'us' or 'eu'
 processor_id = "9db77bc6dadc8719"  # Create processor before running sample
 processor_version = "pretrained-ocr-v1.2-2022-11-10"
-file_path = "/Users/sheresaidon/Downloads/Bloodwork_PII.pdf"
-# file_path = "/Users/sheresaidon/Downloads/test2.pdf"
+# file_path = "/Users/sheresaidon/Downloads/Bloodwork_PII.pdf"
+file_path = "/Users/sheresaidon/Downloads/nish/Provider Notes from ER visit.pdf"
 
+# file_path = "/Users/sheresaidon/Downloads/2069394423-1098-1_1_2023.pdf"
+input_pdf_path = file_path
+file_name, file_extension = os.path.splitext(file_path)
+output_pdf_path = f"{file_name}_annotated{file_extension}"
 mime_type = "application/pdf"  # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
 enable_native_pdf_parsing = True
 
